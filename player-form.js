16c77e8c8de3a3bbfbaf3ea60af028a95e378ec4
toneloke <guerrerotonyjs@gmail.com>
2017-09-11 16:59:11 -0400

    added player forms

--- player-form.js
+++ player-form.js
@@ -1,19 +1,25 @@
+
 class PlayerForm {
-  constructor (player) {
-    this.pForm = document.getElementById(player)
-    // this.player2form = document.getElementById('player2')
+  constructor (playerNum) {
+    this.pForm = document.getElementById(playerNum)
+    this.player = playerNum
+    this.render()
+    this.pForm.addEventListener('click', this.addPlayer.bind(this))
+  }
+  addPlayer () {
+    console.log(new Player(document.getElementsByClassName(this.player)[0].value, document.getElementsByClassName(this.player)[1].value))
   }
   renderForm () {
     return `<form>
       <div class="row">
         <div class="input-field col s6">
           <i class="material-icons prefix">account_circle</i>
-          <input id="icon_prefix" type="text" class="validate">
+          <input class="${this.player}" id="icon_prefix" type="text" >
           <label for="icon_prefix">Name</label>
         </div>
         <div class="input-field col s6">
           <i class="material-icons prefix">apps</i>
-          <input id="icon_telephone" type="tel" class="validate">
+          <input class="${this.player}" id="icon_telephone" type="text" >
           <label for="icon_telephone">Symbol</label>
         </div>
       </div>
@@ -24,23 +30,14 @@ class PlayerForm {
     // this.player1form.innerHTML = this.renderForm()
   }
 }
+let playerOne = new PlayerForm('player1')
+let playerTwo = new PlayerForm('player2')
 
-// let appendForm = new PlayerForm()
-// appendForm.render()
-
-class Player extends PlayerForm {
-  constructor (name, symbol, playerNum) {
-    super(playerNum)
+class Player {
+  constructor (name, symbol) {
     this.name = name
     this.symbol = symbol
     this.totalScore = 0
-    this.pForm.addEventListener('click', this.addPlayer.bind(this))
-  }
-  addPlayer () {
-    event.preventDefault()
   }
 
 }
-
-let Tony = new Player('tony', 'T', 'player1')
-Tony.render()
